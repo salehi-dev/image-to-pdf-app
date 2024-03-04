df4682998876c9057d02d1fcf29907e6ca499f66
Aref Salehi <m.arf.salehi@gmail.com>
2024-03-04 23:46:20 +0330

    Imlement the structure

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+} from "react-native";
 import { printToFileAsync } from "expo-print";
 import { shareAsync } from "expo-sharing";
 
 export default function App() {
+  let [name, setName] = useState("");
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>Hello World</Text>
+        <TextInput
+          onChangeText={(value) => setName(value)}
+          value={name}
+          placeholder="Name"
+          style={styles.input}
+        />
+        <Button title="Generate PDF" />
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -21,5 +36,14 @@ const styles = StyleSheet.create({
     margin: 20,
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    width: 290,
+    height: 45,
+    padding: 10,
+    borderRadius: 6,
+    borderWidth: 1,
+    marginBottom: 20,
+    borderColor: "rgba(0, 0, 0, 0.5)",
   },
 });
